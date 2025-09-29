6c055814930743208e0237c032a50d3bd3bb4dcb
sotrali <vesspa0330@gmail.com>
2025-09-28 21:15:56 -0400

    new project structure, faq adj, review fix

--- js/dropdown.js
+++ js/dropdown.js
@@ -2,6 +2,7 @@ function toggleDropdown(headerClicked) {
   let body = headerClicked.nextElementSibling;
   let arrow = headerClicked.children[1];
 
+  headerClicked.classList.toggle("open");
   body.classList.toggle("open");
   arrow.classList.toggle("open");
 }
